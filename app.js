a0cad2b9bc93537f841a57c8a51cfeaaca008864
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-05-20 16:09:18 +0100

    take 2 number and operator and calculate

--- app.js
+++ app.js
@@ -12,62 +12,111 @@ const button8 = document.getElementById("button-8");
 const button9 = document.getElementById("button-9");
 const buttonClear = document.getElementById("button-clear");
 const buttonEqual = document.getElementById("button-equal");
-//const operators = document.querySelector(".button-operation")
+const operators = document.querySelectorAll(".button-operation")
 const buttonPlus = document.getElementById("button-plus");
+const buttonSubtract = document.getElementById("button-subtract");
+const buttonMultiply = document.getElementById("button-multiply");
+const buttonDivide = document.getElementById("button-divide");
 
-const result = 12064;
-const calculation = "1 + 2 =";
+console.log(operators)
+
+const result = "0";
+const calculation = "";
 const value = [];
-
-/////Psuedo Code
-//if (operator = "add")
-//else if (operator = "subtract")
-//else if (operator = "multiply")
-//else if (operator = "divide")
-
-const onClick0 = (event) => {
-    console.log(0);
-};
-
-const onClick1 = (event) => {
-    //return value.push(1);
-    console.log(1);
-};
-const onClick2 = (event) => {
-    console.log(2);
-};
-const onClick3 = (event) => {
-    console.log(3);
-};
-const onClick4 = (event) => {
-    console.log(4);
-};
-const onClick5 = (event) => {
-    console.log(5);
-};
-const onClick6 = (event) => {
-    console.log(6);
-};
-const onClick7 = (event) => {
-    console.log(7);
-};
-const onClick8 = (event) => {
-    console.log(8);
-};
-const onClick9 = (event) => {
-    console.log(9);
-};
-
-const onClickClear = (event) => {
-    console.log(0);
-};
-
-const onClickEqualsCalculate = (event) => {
-    console.log("Now calculating");
-};
+const num1 = "";
+const num2 = "";
+const operator = "";
 
 resultHeading.innerText = result;
 calculationHeading.innerText = calculation;
+/*
+const onClickoperators = (event) => {
+    console.log("I am an operator");
+};
+*/
+
+const onClick0 = (event) => {
+    calculationHeading.innerText += "0";
+};
+
+const onClick1 = (event) => {
+    calculationHeading.innerText += "1";
+    //console.log(1);
+};
+const onClick2 = (event) => {
+    calculationHeading.innerText += "2";
+    //console.log(2);
+};
+const onClick3 = (event) => {
+    calculationHeading.innerText += "3";
+    //console.log(3);
+};
+const onClick4 = (event) => {
+    calculationHeading.innerText += "4";
+    //console.log(4);
+};
+const onClick5 = (event) => {
+    calculationHeading.innerText += "5";
+    //console.log(5);
+};
+const onClick6 = (event) => {
+    calculationHeading.innerText += "6";
+    //console.log(6);
+};
+const onClick7 = (event) => {
+    calculationHeading.innerText += "7";
+    //console.log(7);
+};
+const onClick8 = (event) => {
+    calculationHeading.innerText += "8";
+    //console.log(8);
+};
+const onClick9 = (event) => {
+    calculationHeading.innerText += "9";
+    //console.log(9);
+};
+
+const onClickPlus = (event) => {
+    calculationHeading.innerText += "+";
+    //console.log(9);
+};
+
+const onClickSubtract = (event) => {
+    calculationHeading.innerText += "-";
+    //console.log(9);
+};
+
+const onClickMultiply = (event) => {
+    calculationHeading.innerText += "x";
+    //console.log(9);
+};
+
+const onClickDivide = (event) => {
+    calculationHeading.innerText += "÷";
+    //console.log(9);
+};
+
+const onClickClear = (event) => {
+    resultHeading.innerText = "0";
+    calculationHeading.innerText = "";
+    //console.log(0);
+};
+
+const onClickEqualsCalculate = (event) => {
+    //console.log(calculationHeading.innerText);
+     if(calculationHeading.innerText.includes("+")){
+        splitStringPlus();
+    }else if (calculationHeading.innerText.includes("-")){
+        splitStringSubtract();
+    }else if (calculationHeading.innerText.includes("x")){
+        splitStringMultiply();
+    }else if (calculationHeading.innerText.includes("÷")){
+        splitStringDivide();
+    }
+    
+    //calculate(3, "+", 67);
+};
+
 
 button0.addEventListener("click", onClick0);
 button1.addEventListener("click", onClick1);
@@ -79,20 +128,43 @@ button6.addEventListener("click", onClick6);
 button7.addEventListener("click", onClick7);
 button8.addEventListener("click", onClick8);
 button9.addEventListener("click", onClick9);
+buttonPlus.addEventListener("click", onClickPlus);
+buttonSubtract.addEventListener("click", onClickSubtract);
+buttonMultiply.addEventListener("click", onClickMultiply);
+buttonDivide.addEventListener("click", onClickDivide);
 buttonClear.addEventListener("click", onClickClear);
 buttonEqual.addEventListener("click", onClickEqualsCalculate);
+//operators.addEventListener("click", onClickoperators);
 
-const calculate = (num1, operator, num2) => {
-    let result = 0;
-    if (operator === "add") {
-        result = num1 + num2;
-    }else if (operator === "subtract") {
-        result = num1 - num2;
-    }else if (operator === "multiply") {
-        result = num1 * num2;
-    }else if (operator === "divide") {
-        result = num1 / num2;
-    }
-    console.log(result);
+
+const splitStringPlus = (string) => {
+    const plusArray = calculationHeading.innerHTML.split("+")
+    const plusArrayNumbers = plusArray.map(string => Number(string));
+    const sumOfArray = plusArrayNumbers.reduce((runningTotal, currentVal) => {
+        return runningTotal + currentVal
+    })
+    resultHeading.innerText = sumOfArray;
 }
-calculate(3, "add", 67);
+
+const splitStringSubtract = (string) => {
+    const subtractArray = calculationHeading.innerHTML.split("-")
+    const subtractArrayNumbers = subtractArray.map(string => Number(string));
+    let total = subtractArrayNumbers[0] - subtractArrayNumbers[1];
+    resultHeading.innerText = total;
+}
+
+const splitStringMultiply = (string) => {
+    const subtractArray = calculationHeading.innerHTML.split("x")
+    const subtractArrayNumbers = subtractArray.map(string => Number(string));
+    let total = subtractArrayNumbers[0] * subtractArrayNumbers[1];
+    resultHeading.innerText = total;
+}
+
+const splitStringDivide = (string) => {
+    const subtractArray = calculationHeading.innerHTML.split("÷")
+    const subtractArrayNumbers = subtractArray.map(string => Number(string));
+    let total = subtractArrayNumbers[0] / subtractArrayNumbers[1];
+    resultHeading.innerText = total;
+}
+
+
